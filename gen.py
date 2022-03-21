6666d0bdc08851b70bf60a7e20da1b34ed837b0a
Thibault Raffaillac <traf@kth.se>
2022-03-21 09:21:01 +0100

    Dernières mises à jour

--- gen.py
+++ gen.py
@@ -1,3 +1,7 @@
+# TODO :
+# _ convert all sizes to em and reduce base size by 90%
+# _ for Teaching, replace institutions by their logos
+
 from datetime import date
 from json import load
 from random import randint
@@ -52,7 +56,8 @@ header_projects = '''\n\n\n<h1 id=projects style="box-shadow: inset 0 -50px 50px
 	As a consequence I do few projects and spend years on some, but hopefully and eventually they become useful to people.
 	The projects below took a significant share of my time and energy.
 </p>'''
-header_publications = '\n\n\n<h1 id=publications style="box-shadow: inset 0 -50px 50px -50px #e09f3e; margin-top: 100px">Publications</h1>\n<div class=references>'
+header_publications = '''\n\n\n<h1 id=publications style="box-shadow: inset 0 -50px 50px -50px #e09f3e; margin-top: 100px">Publications</h1>
+<div class=references>'''
 # FIXME je participe souvent à la rédaction des TDs
 header_teaching = '''\n\n\n<h1 id=teaching style="box-shadow: inset 0 -50px 50px -50px #540b0e; margin-top: 100px">Teaching</h1>
 <div class=courses>
@@ -61,9 +66,10 @@ header_teaching = '''\n\n\n<h1 id=teaching style="box-shadow: inset 0 -50px 50px
 	<h3><b>Level</b></h3>
 	<h3><b>Institution</b></h3>
 	<h3><b>Hours</b></h3>'''
-# FIXME academic duties avec #fff3b0
+header_duties = '''\n\n\n<h1 id=duties style="box-shadow: inset 0 -50px 50px -50px #ffd200; margin-top: 100px">Academic duties</h1>
+<div class=duties>'''
 header_misc = '''\n\n\n<h1 id=misc style="box-shadow: inset 0 -50px 50px -50px #9e2a2b; margin-top: 100px">Signs of life</h1>
-<p style="margin: 20px 175px">
+<p style="margin: 20px 175px 15px 175px">
 	This section contains stuff that doesn't fit anywhere else.
 	I do a lot of challenges (with or without programming), to keep myself mentally fit and just have fun from time to time.
 	My main platform for contests training is <a href=https://open.kattis.com/countries/FRA>Kattis</a>, where I hold 2<sup>nd</sup> place in France.
@@ -94,12 +100,13 @@ with open('index.html', 'w') as f:
 	for e in events:
 		if e["type"] == 'project':
 			h3 = f'<a href={e["href"]}>{e["title"]}</a>' if "href" in e else e["title"]
-			interval = e["start"] if e["start"]==e.get("finish",0) else f'{e["start"]}-{e.get("finish", "")}'
+			period = f'{e["start"]}-{e.get("finish","")}' if "start" in e else e["finish"]
 			bg = 'background-color: rgb(51, 92, 103, 0.2); ' if 'major' in e else ''
-			print(f'<div class=card style="{bg}margin-left: {10+pos}px; margin-right: {340-pos}px">', file=f)
+			Id = f' id={e["id"]}' if "id" in e else ''
+			print(f'<div{Id} class=card style="{bg}margin-left: {10+pos}px; margin-right: {340-pos}px">', file=f)
 			print(f'\t<img src=images/{e["image"]}>', file=f)
 			print('\t<div class=flexcolstretch>', file=f)
-			print(f'\t\t<h3>{h3} ({interval})</h3>', file=f)
+			print(f'\t\t<h3>{h3} ({period})</h3>', file=f)
 			print(f'\t\t<h4>{e["text"]}</h4>', file=f)
 			print('\t</div>', file=f)
 			print('</div>', file=f)
@@ -108,32 +115,43 @@ with open('index.html', 'w') as f:
 	print(header_publications, file=f)
 	for e in events:
 		if e["type"] == 'publication':
+			Id = f' id={e["id"]}' if "id" in e else ''
 			bg = ' style="background-color: rgb(224, 159, 62, 0.2)"' if 'major' in e else ''
-			print(f'\t<h3 id={e["id"]}{bg}>{e["reference"]}</h3>' if "id" in e else f'\t<h3>{e["reference"]}</h3>', file=f)
+			print(f'\t<h3{Id}{bg}>{e["reference"]}</h3>', file=f)
 			print(f'\t<h4{bg}>{e["text"]}</h4>', file=f)
 	print('</div>', file=f)
 	# section Teaching
 	print(header_teaching, file=f)
 	for e in events:
 		if e["type"] == 'teaching':
-			print(f'\t<h4>{e["start"]}-{e["finish"]}</h4>', file=f)
+			period = f'{e["start"]}-{e.get("finish","")}' if "start" in e else e["finish"]
+			Id = f' id={e["id"]}' if "id" in e else ''
+			print(f'\t<h4 style="text-align: center">{period}</h4>', file=f)
 			print('\t<div>', file=f)
-			print(f'\t\t<h4><b>{e["title"]} ({e["role"]})</b></h4>', file=f)
+			print(f'\t\t<h4{Id}><b>{e["title"]} ({e["role"]})</b></h4>', file=f)
 			print(f'\t\t<h4>{e["text"]}</h4>', file=f)
 			print('\t</div>', file=f)
 			print(f'\t<h4>{e["level"]}</h4>', file=f)
 			print(f'\t<h4>{e["institution"]}</h4>', file=f)
-			print(f'\t<h4>{e["hours"]}</h4>', file=f)
+			print(f'\t<h4 style="text-align: center">{e["hours"]}</h4>', file=f)
+	print('</div>', file=f)
+	# section Academic duties
+	print(header_duties, file=f)
+	for e in events:
+		if e["type"] == 'duty':
+			Id = f' id={e["id"]}' if "id" in e else ''
+			print(f'<h3{Id}>{e["text"]}</h3>', file=f)
 	print('</div>', file=f)
 	# section Signs of life
 	print(header_misc, file=f)
-	year = date.today().year
+	year = None
 	for e in events:
 		if e["type"] == 'misc':
 			if e["finish"] != year:
 				year = e["finish"]
 				print(f'<h2 style="color: #ddd; margin-top: 25px; text-align: center">{year}</h2>', file=f)
-			print('<div class=sign>', file=f)
+			Id = f' id={e["id"]}' if "id" in e else ''
+			print(f'<div{Id} class=sign>', file=f)
 			print(f'\t<h3><b>{e["title"]}</b></h3>', file=f)
 			print(f'\t<h4>{e["text"]}</h4>', file=f)
 			print('</div>', file=f)
@@ -154,29 +172,33 @@ with open('timeline.html', 'w') as f:
 			finish = e.get('finish', year)
 			# ... s'il est à cheval sur l'année en cours on l'affiche
 			if e.get('start', finish) <= year <= finish:
-				print(f'<div id={e["id"]} class=strip>' if "id" in e else '<div class=strip>', file=f)
+				Id = f' id={e["id"]}' if "id" in e else ''
+				print(f'<div{Id} class=strip>', file=f)
+				period = f'{e["start"]}-{e.get("finish","")}' if "start" in e else e["finish"]
 				if etype == 'project':
 					h3 = f'<a href={e["href"]}>{e["title"]}</a>' if "href" in e else e["title"]
-					interval = e["start"] if e["start"]==e.get("finish",0) else f'{e["start"]}-{e.get("finish", "")}'
 					print('\t<div class=project>project</div>', file=f)
 					print(f'\t<img src=images/{e["image"]} width=300>', file=f)
 					print('\t<div class=flexcolstretch style="justify-content: center">', file=f)
-					print(f'\t\t<h3>{h3} ({interval})</h3>', file=f)
+					print(f'\t\t<h3>{h3} ({period})</h3>', file=f)
 					print(f'\t\t<h4>{e["text"]}</h4>', file=f)
 					print('\t</div>', file=f)
 				elif etype == 'publication':
-					print(f'\t<div class=publication>publication</div>', file=f)
+					print('\t<div class=publication>publication</div>', file=f)
 					print(f'\t<h3>{e["reference"]}</h3>', file=f)
 					print(f'\t<h4>{e["text"]}</h4>', file=f)
 				elif etype == 'teaching':
-					print(f'\t<div class=teaching>teaching</div>', file=f)
+					print('\t<div class=teaching>teaching</div>', file=f)
 					print('\t<div class=flexcolstretch style="justify-content: center">', file=f)
-					print(f'\t\t<h3>{e["title"]} ({e["start"]}-{e["finish"]})</h3>', file=f)
+					print(f'\t\t<h3>{e["title"]} ({period})</h3>', file=f)
 					print(f'\t\t<h4><i>{e["role"]}</i> at <i>{e["level"]}</i> level for <i>{e["hours"]}h</i> at <i>{e["institution"]}</i></h4>', file=f)
 					print(f'\t\t<h4>{e["text"]}</h4>', file=f)
 					print('\t</div>', file=f)
+				elif etype == 'duty':
+					print('\t<div class=duty>academic duty</div>', file=f)
+					print(f'<h4>{e["text"]}</h4>', file=f)
 				elif etype == 'misc':
-					print(f'\t<div class=misc>sign of life</div>', file=f)
+					print('\t<div class=misc>sign of life</div>', file=f)
 					print('\t<div class=flexcolstretch style="justify-content: center">', file=f)
 					print(f'\t\t<h3>{e["title"]}</h3>', file=f)
 					print(f'\t\t<h4>{e["text"]}</h4>', file=f)
